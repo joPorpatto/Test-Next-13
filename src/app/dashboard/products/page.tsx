92cf60e184ad397cc99213b0a17e8a4b3957fb6c
joPorpatto <joel.porpatto@gmail.com>
2024-05-18 14:03:54 -0300

    the entire cookies section is added

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,12 @@
+import { products } from "@/products/data/product";
+import { ProductCard } from "../../../products/components/ProductCard";
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </div>
+  );
+}
